c6fd55fd5515f65c606847fe6ddb978bc6031967
Azmir Shah <azmirshah1@gmail.com>
2019-04-15 23:36:23 +0800

    Update testproject.sql

--- testproject.sql
+++ testproject.sql
@@ -1,33 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 15, 2019 at 05:36 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.10
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-CREATE TABLE `employeedetail` (
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `testproject`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
   `Id` int(11) NOT NULL,
-  `EmpFname` varchar(50) DEFAULT NULL,
-  `EmpLName` varchar(50) NOT NULL,
-  `EmpSecurityCode` varchar(50) DEFAULT NULL,
-  `EmpContactNo` bigint(10) DEFAULT NULL,
-  `EmpGender` enum('Male','Female') DEFAULT NULL,
-  `EmpEmail` varchar(200) DEFAULT NULL,
-  `EmpPassword` varchar(100) DEFAULT NULL,
+  `AdminName` varchar(50) DEFAULT NULL,
+  `AdminUserName` varchar(50) DEFAULT NULL,
+  `Password` varchar(45) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `admin`
+--
 
-ALTER TABLE `employeedetail`
-  ADD PRIMARY KEY (`Id`),
-  ADD UNIQUE KEY `EmpSecurityCode` (`SecurityCode`);
+INSERT INTO `admin` (`Id`, `AdminName`, `AdminUserName`, `Password`, `AdminRegdate`) VALUES
+(1, 'Sarita Pandey', 'Admin', 'Test@123', '2019-02-07 08:52:45'),
+(2, 'Azmir Shah', 'askcoreunit', 'raider20', '2019-04-15 15:14:44');
 
+-- --------------------------------------------------------
 
-
-ALTER TABLE `employeedetail`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-
-
--------------------------------------------------------------------------------------------------  
-
+--
+-- Table structure for table `empeducation`
+--
 
 CREATE TABLE `empeducation` (
   `Id` int(11) NOT NULL,
@@ -50,20 +71,14 @@ CREATE TABLE `empeducation` (
   `PercentageHSC` varchar(4) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
-ALTER TABLE `empeducation`
-  ADD PRIMARY KEY (`Id`);
-
-
- ALTER TABLE `empeducation`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-  
-
-  ------------------------------------------------------------------------------------ 
-
+--
+-- Table structure for table `empexpireince`
+--
 
 CREATE TABLE `empexpireince` (
-  `ID` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
   `EmpID` varchar(5) DEFAULT NULL,
   `Employer1Name` varchar(75) DEFAULT NULL,
   `Employer1Designation` varchar(50) DEFAULT NULL,
@@ -79,47 +94,88 @@ CREATE TABLE `empexpireince` (
   `Employer3WorkDuration` varchar(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
 
+--
+-- Table structure for table `employeedetail`
+--
 
-ALTER TABLE `empexpireince`
-  ADD PRIMARY KEY (`Id`);
-
-
-
-ALTER TABLE `empexpireince`
-  ADD PRIMARY KEY (`Id`); 
-
-
-ALTER TABLE `empexpireince`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
-
-------------------------------------------------------------------------------------------
-
-
-
-CREATE TABLE `admin` (
+CREATE TABLE `employeedetail` (
   `Id` int(11) NOT NULL,
-  `AdminName` varchar(50) DEFAULT NULL,
-  `AdminUserName` varchar(50) DEFAULT NULL,
-  `Password` varchar(45) DEFAULT NULL,
-  `AdminRegdate` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+  `EmpFname` varchar(50) DEFAULT NULL,
+  `EmpLName` varchar(50) NOT NULL,
+  `EmpSecurityCode` varchar(50) DEFAULT NULL,
+  `EmpContactNo` bigint(10) DEFAULT NULL,
+  `EmpGender` enum('Male','Female') DEFAULT NULL,
+  `EmpEmail` varchar(200) DEFAULT NULL,
+  `EmpPassword` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `employeedetail`
+--
 
+INSERT INTO `employeedetail` (`Id`, `EmpFname`, `EmpLName`, `EmpSecurityCode`, `EmpContactNo`, `EmpGender`, `EmpEmail`, `EmpPassword`) VALUES
+(14, 'Cubaan', 'bapak', '12345', NULL, NULL, 'cubaan@gmail.com', 'cubaan');
 
-INSERT INTO `admin` (`Id`, `AdminName`, `AdminUserName`, `Password`, `AdminRegdate`) VALUES
-(1, 'Sarita Pandey', 'Admin', 'Test@123', '2019-02-07 16:52:45');
+--
+-- Indexes for dumped tables
+--
 
-
-
-
+--
+-- Indexes for table `admin`
+--
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`Id`);
 
+--
+-- Indexes for table `empeducation`
+--
+ALTER TABLE `empeducation`
+  ADD PRIMARY KEY (`Id`);
 
+--
+-- Indexes for table `empexpireince`
+--
+ALTER TABLE `empexpireince`
+  ADD PRIMARY KEY (`Id`);
 
+--
+-- Indexes for table `employeedetail`
+--
+ALTER TABLE `employeedetail`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `EmpSecurityCode` (`EmpSecurityCode`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
 ALTER TABLE `admin`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `empeducation`
+--
+ALTER TABLE `empeducation`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `empexpireince`
+--
+ALTER TABLE `empexpireince`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `employeedetail`
+--
+ALTER TABLE `employeedetail`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
